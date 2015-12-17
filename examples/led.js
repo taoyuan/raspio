253278c344be958bf9e4ddca5951e2fdeb06f815
taoyuan <torworx@gmail.com>
2015-12-17 16:41:09 +0800

    Implemented led

---
+++ examples/led.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var async = require('async');
+var wpi = require('../').wpi;
+var LED = require('../').LED;
+
+wpi.setup('wpi');
+
+var led = new LED([0, 1, 2], true);
+
+led.brightness(10);
+
+async.series([
+  function (cb) {
+    led.light(LED.red);
+    setTimeout(cb, 2000);
+  },
+  function (cb) {
+    led.light(LED.green);
+    setTimeout(cb, 2000);
+  },
+  function (cb) {
+    led.light(LED.blue);
+    setTimeout(cb, 2000);
+  },
+  function (cb) {
+    led.blink(LED.cyan, 500);
+    setTimeout(cb, 5000);
+  },
+  function (cb) {
+    led.fadeup(LED.white, 10);
+    setTimeout(cb, 5000);
+  },
+  function (cb) {
+    led.fadedown(LED.white, 10);
+    setTimeout(cb, 5000);
+  },
+  function (cb) {
+    led.breath(LED.green, 10);
+    setTimeout(cb, 5000);
+  }
+], function () {
+});
+
+
--- examples/led/blink.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var LED = require('../../').LED;
-
-var led = new LED([0, 1, 2]);
-led.blink(0xFFFFFF);
