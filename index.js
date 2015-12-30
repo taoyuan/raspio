427fe02efc40c08b9c59bf1646cd8f537df4fe5a
taoyuan <torworx@gmail.com>
2015-12-30 19:08:53 +0800

    Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 
-exports.LED = exports.Led = require('./lib/led');
-exports.Button = require('./lib/button');
-exports.Digital = require('./lib/digital');
+exports.led = exports.LED = exports.Led = require('./lib/led');
+exports.button = exports.Button = require('./lib/button');
+exports.digital = exports.Digital = require('./lib/digital');
